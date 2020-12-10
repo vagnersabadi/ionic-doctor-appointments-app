9ec20e794e8453b0901b8e1cc5f36b6e8226e16d
Vagner <vagner@odig.net>
2020-12-09 22:18:59 -0300

    feat: create list doctors

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,5 +1,13 @@
 import { Component } from '@angular/core';
 
+
+interface Doctor {
+  avatar: string;
+  name: string;
+  specialty: string;
+  status: boolean;
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,6 +15,32 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  slideOpts = {
+    initialSlide: 1,
+    speed: 400
+  };
+
+  doctors: Doctor[] = [
+  {
+    avatar: 'https://avatars0.githubusercontent.com/u/12532889?s=400&u=758efee3e15b5e61f684a351fc82048426228f11&v=4',
+    specialty: 'Cardiologist',
+    name: 'Dr. Alan C Braverman',
+    status: true
+  },
+  {
+    avatar: 'https://avatars0.githubusercontent.com/u/12532889?s=400&u=758efee3e15b5e61f684a351fc82048426228f11&v=4',
+    specialty: 'Orthodontist',
+    name: 'Dr. Michael Aaron',
+    status: false
+  },
+  {
+    avatar: 'https://avatars0.githubusercontent.com/u/12532889?s=400&u=758efee3e15b5e61f684a351fc82048426228f11&v=4',
+    specialty: 'Eye Specialist',
+    name: 'Dr. Shelley Rahman',
+    status: false
+  }
+  ]
+
+constructor() { }
 
 }

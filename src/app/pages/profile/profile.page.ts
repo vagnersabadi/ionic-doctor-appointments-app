079fd98bb0dd379e12d0e818e14aaedf209d7d41
Vagner <vagnerbuzatta@hotmail.com>
2021-01-25 21:32:11 -0300

    feat: add about avatar profile

--- src/app/pages/profile/profile.page.ts
+++ src/app/pages/profile/profile.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilePage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private modalController: ModalController
+  ) { }
 
   ngOnInit() {
   }
 
+  close(){
+    this.modalController.dismiss();
+  }
 }
